1d78ade5ae075cd79fadc89bc10c1da2367a1152
TANG FANGRUI <fangrui.tang@biomind.ai>
2022-10-28 11:24:46 +0800

    react lazy suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 import "./App.css";
 import Modal from "./component/Modal";
+import logo from "./logo.svg";
 
+const LazyLoding = React.lazy(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return import("./component/LazyLoding");
+});
 function App() {
+    // Modal
     const [isOpen, setIsOpen] = useState(false);
     function toggleOpen() {
         setIsOpen((prevState) => !prevState);
@@ -12,6 +18,9 @@ function App() {
     }
     return (
         <>
+            <Suspense fallback={<img src={logo} alt="" />}>
+                <LazyLoding></LazyLoding>
+            </Suspense>
             <button onClick={() => toggleOpen()}>custom modal</button>
             <Modal
                 open={isOpen}
