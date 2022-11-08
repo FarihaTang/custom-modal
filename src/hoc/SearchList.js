21732dbc0dc3f04dc6d7490df6f0965217f60294
TANG FANGRUI <fangrui.tang@biomind.ai>
2022-11-08 14:41:23 +0800

    implement hoc with function component

--- src/hoc/SearchList.js
+++ src/hoc/SearchList.js
@@ -1,26 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function SearchListHOC(WrappedComponent, entity) {
-    return class extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                data: [],
-                term: "",
-            };
-        }
-        componentDidMount() {
+    return function SearchListHOC(props) {
+        const [data, setData] = useState([]);
+        const [term, setTerm] = useState("");
+        const [filteredData, setFilteredData] = useState([]);
+        useEffect(() => {
             const fetchData = async () => {
                 const response = await fetch(
                     `http://jsonplaceholder.typicode.com/${entity}`
                 );
                 const data = await response.json();
-                this.setState({ ...this.state, data });
+                setData(data);
             };
             fetchData();
-        }
-        render() {
-            const { data, term } = this.state;
+        }, []);
+        useEffect(() => {
             let filteredData = data.filter((d) => {
                 if (entity === "users") {
                     return d.name.includes(term);
@@ -28,23 +23,64 @@ function SearchListHOC(WrappedComponent, entity) {
                     return d.title.includes(term);
                 }
             });
-            return (
-                <div>
-                    <h3>{entity}</h3>
-                    <input
-                        type="text"
-                        value={term}
-                        onChange={(e) =>
-                            this.setState({
-                                ...this.state,
-                                term: e.target.value,
-                            })
-                        }
-                    />
-                    <WrappedComponent data={filteredData}></WrappedComponent>
-                </div>
-            );
-        }
+            setFilteredData(filteredData);
+        }, [data, term]);
+        return (
+            <div>
+                <h3>{entity}</h3>
+                <input
+                    type="text"
+                    value={term}
+                    onChange={(e) => setTerm(e.target.value)}
+                />
+                <WrappedComponent data={filteredData}></WrappedComponent>
+            </div>
+        );
     };
+    // return class extends React.Component {
+    //     constructor(props) {
+    //         super(props);
+    //         this.state = {
+    //             data: [],
+    //             term: "",
+    //         };
+    //     }
+    //     componentDidMount() {
+    //         const fetchData = async () => {
+    //             const response = await fetch(
+    //                 `http://jsonplaceholder.typicode.com/${entity}`
+    //             );
+    //             const data = await response.json();
+    //             this.setState({ ...this.state, data });
+    //         };
+    //         fetchData();
+    //     }
+    //     render() {
+    //         const { data, term } = this.state;
+    //         let filteredData = data.filter((d) => {
+    //             if (entity === "users") {
+    //                 return d.name.includes(term);
+    //             } else if (entity === "posts") {
+    //                 return d.title.includes(term);
+    //             }
+    //         });
+    //         return (
+    //             <div>
+    //                 <h3>{entity}</h3>
+    //                 <input
+    //                     type="text"
+    //                     value={term}
+    //                     onChange={(e) =>
+    //                         this.setState({
+    //                             ...this.state,
+    //                             term: e.target.value,
+    //                         })
+    //                     }
+    //                 />
+    //                 <WrappedComponent data={filteredData}></WrappedComponent>
+    //             </div>
+    //         );
+    //     }
+    // };
 }
 export default SearchListHOC;
